79642fb59b9c0c857f2504c48a449f5c39e756d7
DEXTER <144078621+purnapurna2007@users.noreply.github.com>
2024-05-02 22:54:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Unique md pairing code
+- Kindly star my repo
+- Fork and edit as you wish
+- Deploy to your favourite hosting server eg Heroku or Render or self hosting
+
+<strong>NB:<strong/> This repo also generates session ID for all bots using whiskeysockets/baileys
+
+# kindly fork the repo now
+use the below button to fork the repo and edit for session id for your bot
+
+***Click [`FORK`](https://github.com/Kingjux/UNIQUE-PAIR/fork) and `Star ⭐ Repository` for Courage.***
+
+# now heroku deploy ✍️👋
+deploy your session id to heroku below 👇👇👇👇
+
+***Deploy on [`HEROKU`](https://dashboard.heroku.com/new?template=https%3A%2F%2Fgithub.com%2Fkingjux%2FUNIQUE-PAIR)***
---
+++ pair.js
@@ -0,0 +1,99 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Venocyber_Tech,    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function UNIQUE_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Venocyber_Tech = Venocyber_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_Venocyber_Tech.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Venocyber_Tech.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Venocyber_Tech.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Venocyber_Tech.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id, { text: '' + b64data });
+
+               let UNIQUE_MD_TEXT = `
+*_Pair Code Connected by  𝗱𝗲𝘅𝘁𝗲𝗿-md_*
+*_Made With 🤍_*
+______________________________________
+╔════◇
+║ *『 𝗪𝗘𝗟𝗖𝗢𝗠𝗘 』*
+║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+╚══════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Youtube:* _https://youtube.com/@Dextertoola999_
+║❒ *Owner:* _https://wa.me/message/LLGU3TSNGKH6J1_
+║❒ *Repo:* _https://github.com/DEXTER-BOT-1/DEXTER-V1-MD_
+║❒ *WaGroup:* _https://chat.whatsapp.com/HjTJEJm6IX14AxUCsqfOQJ_
+║❒ *WaChannel:* https://whatsapp.com/channel/0029VaZZclvLtOjKGav75L1B
+║❒ *𝗗𝗘𝗫𝗧𝗘𝗥 MD:* 
+╚══════════════════════╝ 
+_____________________________________
+
+_𝗗𝗘𝗫𝗧𝗘𝗥 𝗠𝗗_`
+ await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id,{text:UNIQUE_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Venocyber_Tech.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    UNIQUE_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await UNIQUE_MD_PAIR_CODE()
+});
+module.exports = router
